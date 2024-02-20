cb1cad2c0d556312713380a0f6cccfa146d205a7
Yidai <yzhang958@connect.hkust-gz.edu.cn>
2024-02-20 10:53:08 +0800

    initial

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
-using MyFirstPackage
 using Test
+using MyFirstPackage
 
-@testset "MyFirstPackage.jl" begin
-    # Write your tests here.
+@testset "greet" begin
+    @test greet("Julia") == "Hello, Julia!"
+end
+
+@testset "private sum" begin
+    # because we have not exported the `private_sum` function,
+    # we need to use the full path to call it
+    @test MyFirstPackage.private_sum([1, 2, 3]) == 6
+    @test MyFirstPackage.private_sum(Int[]) == 0
 end
