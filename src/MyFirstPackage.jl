94781b206d994186b8f58ad4ba5dc27874df2256
Yidai <yzhang958@connect.hkust-gz.edu.cn>
2024-02-27 15:16:33 +0800

    update

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -6,8 +6,12 @@ using LinearAlgebra
 export Lorenz, integrate_step
 export Point, Point2D, Point3D
 export RungeKutta, Euclidean
+export equilibrium_density, collide, example_d2q9, velocity, flip_direction_index, stream!, curl, LatticeBoltzmann, step!
 
 # `include` other source files into this module
 include("lorenz.jl")
 
+include("fluid.jl")
+
 end
+
